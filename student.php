9c38641988bb1c1c878c10f39ca90ab22d0a6d68
THEJAS KUMAR.B <84064959+thejas1508@users.noreply.github.com>
2023-08-22 19:13:51 +0530

    student.php

---
+++ student.php
@@ -0,0 +1,23 @@
+<html>
+<head></head>
+<body>
+<?php
+  $a=array(array(10,"AAA","IT.60)array(20,"BBB","80",)30,"CCC","iv",40));
+  echo"<table border='i'>";
+  echo"<tr>";
+  echo"<td>ENo</td><td>name</td><td>sem</td><td>marks</td>";
+  echo"</tr>";
+ for($i=0;$i<4;$i++){
+  echo "<td>";
+  echo $a[%i][$j];
+  echo "</td>";
+ }
+ echo "<table>";
+  ?>
+</body>
+</html>
+
+
+
+
+
